1479a142493962ed2ca2a8b8a824565fb31f2c43
Martin-Cap <114155164+Martin-Cap@users.noreply.github.com>
2022-10-09 17:06:55 +0200

    Update 1) Rozdělení stringů indexováním.py

---
+++ 1) Rozdělení stringů indexováním.py
@@ -0,0 +1,16 @@
+#Zadaný string
+muj_str = "indexování"
+
+# Prvních 5 znaků
+prvnich_pet = muj_str[:5]
+
+# Posledních 5 znaků
+poslednich_pet = muj_str[5:]
+
+# Každé třetí písmeno od 'i'
+kazde_treti = muj_str[::3]
+
+# Vypsané výsledky s proměnnými
+print("Prvních 5 písmen:", prvnich_pet)
+print("Posledních 5 písmen:", poslednich_pet)
+print("Každé 3. písmeno (počínaje prvním) od 'i':", kazde_treti)
